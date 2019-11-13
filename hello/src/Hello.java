72bb0f49fe95bd0f7b697ecb046db88f608dd765
DjPleata <57720792+DjPleata@users.noreply.github.com>
2019-11-13 20:56:25 +0200

    Add files via upload

---
+++ hello/src/Hello.java
@@ -0,0 +1,14 @@
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("Hello");
+        System.out.println(33 + 2);
+        System.out.println(33.45 / 0.3);
+        System.out.println(100.0%3.0);
+        System.out.println(-100);
+        System.out.println(false);
+        System.out.println(!!false);
+        System.out.println("Hello Java!");
+        System.out.println("H" + "A");
+        System.out.println("Hello" + " " + "A");
+    }
+}
